579e28d7b0e8506da038b314ce8aea45f0ada744
Nguyen Duy <nguyenduyvan.55555@gmail.com>
2021-11-23 23:39:46 +0700

    new update 23/11

---
+++ Assets/Scenes/Room/Scripts/Moving.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    public Transform vrCamera;
+    public float togAngle = 20f;
+    public float speed = 3f;
+    public bool moveForward;
+    private CharacterController controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovingPlayer();
+    }
+    void MovingPlayer()
+    {
+        if (vrCamera.eulerAngles.x >= togAngle && vrCamera.eulerAngles.x < 90)
+        {
+            moveForward = true;
+        }
+        else
+        {
+            moveForward = false;
+        }
+        if (moveForward)
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+            controller.SimpleMove(forward * speed);
+        }
+    }
+}
